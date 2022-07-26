12f266ceda679cf1f43220225cddc5f218f1d3e5
matheus3006 <matheussouza18@live.com>
2022-07-25 21:29:57 -0300

    feat: add Loading screen and theme to App

--- App.tsx
+++ App.tsx
@@ -1,15 +1,30 @@
-import { NativeBaseProvider, StatusBar } from "native-base";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto';
+import { NativeBaseProvider, StatusBar } from 'native-base';
 import React from 'react';
+import { Loading } from './src/Components/Loading';
 
+import { Home } from './src/Screens/Home';
+
+import { THEME } from './src/styles/theme';
 
 export default function App() {
-  return (
-    <NativeBaseProvider> 
-      
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+  });
 
+  return (
+    <NativeBaseProvider theme={THEME}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoaded ? <Home /> : <Loading />}
     </NativeBaseProvider>
-  
   );
 }
-
-
