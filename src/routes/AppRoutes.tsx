9eb22b02c0de70322b152a2912e7aa3210c87b96
matheus3006 <matheussouza18@live.com>
2022-07-27 08:35:24 -0300

    feat: add NewGame screen

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,5 +1,6 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { Home } from "../Screens/Home";
+import { NewGame } from "../Screens/NewGame";
 
 const { Navigator, Screen } = createNativeStackNavigator();
 
@@ -7,6 +8,7 @@ export const AppRoutes = () => {
   return (
     <Navigator screenOptions={{ headerShown: false }}>
       <Screen name="home" component={Home} />
+      <Screen name="newGame" component={NewGame} />
     </Navigator>
   )
 }
