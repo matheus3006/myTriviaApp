a02b48a29e42319fc8cb40b2451998208bb4970e
matheus3006 <matheussouza18@live.com>
2022-07-29 23:42:24 -0300

    feat: implemented useContext to NewGame Screen

--- src/Screens/NewGame.tsx
+++ src/Screens/NewGame.tsx
@@ -1,6 +1,12 @@
 import { VStack } from 'native-base';
+import { useContext } from 'react';
+import { GameContext } from '../context/GameContext';
 
 export const NewGame = ()=> {
+
+  const gameQuestions = useContext(GameContext)
+  console.log(gameQuestions)
+
   return (
     <VStack>
 
